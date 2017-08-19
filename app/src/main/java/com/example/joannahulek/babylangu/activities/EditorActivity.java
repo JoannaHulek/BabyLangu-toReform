cbc3fa1cc9352ae346ef7185eb8750e26326917c
Joanna Hulek <joanna.hulek@gmail.com>
2017-08-19 11:06:06 +0200

    MainCaseActivity Loader

---
+++ app/src/main/java/com/example/joannahulek/babylangu/activities/EditorActivity.java
@@ -0,0 +1,15 @@
+package com.example.joannahulek.babylangu.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.joannahulek.babylangu.R;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
--- app/src/main/java/com/example/joannahulek/babylangu/activities/MainCaseActivity.java
+++ app/src/main/java/com/example/joannahulek/babylangu/activities/MainCaseActivity.java
@@ -1,26 +1,58 @@
 package com.example.joannahulek.babylangu.activities;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import com.example.joannahulek.babylangu.R;
+import com.example.joannahulek.babylangu.adapters.KidCursorAdapter;
+import com.example.joannahulek.babylangu.data.KidContract;
 
 public class MainCaseActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private KidCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_case);
+
+        adapter = new KidCursorAdapter(this, null);
+
+        ListView kidsListView = (ListView) findViewById(R.id.kids_list_view);
+        kidsListView.setEmptyView(findViewById(R.id.empty_view));
+        kidsListView.setAdapter(adapter);
+        getSupportLoaderManager().initLoader(0, null, this);
+
+        FloatingActionButton addFab = (FloatingActionButton) findViewById(R.id.add_fab);
+        addFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainCaseActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        String[] projection = {
+                KidContract.KidEntry._ID,
+                KidContract.KidEntry.TABLE_NAME,
+                KidContract.KidEntry.COLUMN_BIRTH,
+                KidContract.KidEntry.COLUMN_IMG_URI,
+                KidContract.KidEntry.COLUMN_BG_COLOR};
+        return new CursorLoader(this, KidContract.KidEntry.CONTENT_URI, projection, null, null, null);
     }
+
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
